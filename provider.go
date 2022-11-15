17cafff8142154b4ef69006d9f49acb311549549
Luca Campisi <Luca.Campisi@netcetera.com>
2022-11-15 17:11:52 +0100

    Initial

---
+++ provider.go
@@ -0,0 +1,59 @@
+package mssql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"net/url"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"username": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"password": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"host": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"port": {
+				Type:     schema.TypeInt,
+				Default:  1433,
+				Optional: true,
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"mssql_database": resourceDatabase(),
+			"mssql_user":     resourceUser(),
+		},
+		DataSourcesMap:       map[string]*schema.Resource{},
+		ConfigureContextFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	username := d.Get("username").(string)
+	password := d.Get("password").(string)
+	u := &url.URL{
+		Scheme: "sqlserver",
+		User:   url.UserPassword(username, password),
+		Host:   fmt.Sprintf("%s:%d", d.Get("host"), d.Get("port")),
+	}
+
+	db, err := sql.Open("sqlserver", u.String())
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+
+	return db, nil
+
+}
---
+++ resource_user.go
@@ -0,0 +1,146 @@
+package mssql
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceUser() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceUserCreate,
+		ReadContext:   resourceUserRead,
+		UpdateContext: resourceUserUpdate,
+		DeleteContext: resourceUserDelete,
+
+		Schema: map[string]*schema.Schema{
+			"database": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"username": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"password": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"schema": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceUserCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	db := m.(*sql.DB)
+	database := d.Get("database").(string)
+	username := d.Get("username").(string)
+	password := d.Get("password").(string)
+	schema := d.Get("schema").(string)
+
+	row, err := checkLogin(db, username)
+	if err == sql.ErrNoRows {
+
+		_, err := db.Query(fmt.Sprintf("CREATE LOGIN \"%s\" WITH PASSWORD = '%s', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF", username, password))
+		if err != nil {
+			return diag.FromErr(errors.New(fmt.Sprint("Failed to create login", err)))
+		}
+
+		_, err = db.Query(fmt.Sprintf("exec('use %s; CREATE SCHEMA \"%s\"')", database, schema))
+		if err != nil {
+			return diag.FromErr(errors.New(fmt.Sprint("Failed to create user schema: ", err)))
+		}
+
+		_, err = db.Query(fmt.Sprintf("exec('use %s; CREATE USER \"%s\" FOR LOGIN \"%s\" with default_schema = \"%s\"')", database, username, username, schema))
+		//_, err = db.Query(fmt.Sprintf(  "CREATE USER \"%s\" FOR LOGIN \"%s\" with default_schema = dbo", username, username))
+		if err != nil {
+			return diag.FromErr(errors.New(fmt.Sprint("Failed to create user: ", err)))
+		}
+
+	}
+
+	row, err = checkLogin(db, username)
+	if err != nil {
+		return diag.FromErr(errors.New(fmt.Sprint("Unknow error occured:", err)))
+	}
+
+	d.SetId(fmt.Sprint(row.principal_id))
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+
+}
+
+//SELECT [name]
+//FROM [sys].[database_principals]
+//WHERE [type] = N'S'
+
+func resourceUserRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+
+	db := m.(*sql.DB)
+	row := db.QueryRow(fmt.Sprintf("SELECT name FROM master.sys.server_principals WHERE principal_id = %s", d.Id()))
+	var name string
+	err := row.Scan(&name)
+	if err == sql.ErrNoRows {
+		return nil
+	} else if err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("username", name); err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
+
+func resourceUserUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceUserDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	db := m.(*sql.DB)
+	database := d.Get("database").(string)
+	row := db.QueryRow(fmt.Sprintf("SELECT name FROM master.sys.server_principals WHERE principal_id = %s", d.Id()))
+	var name string
+	err := row.Scan(&name)
+
+	if err != sql.ErrNoRows {
+		_, err = db.Query(fmt.Sprintf("DROP LOGIN %s", name))
+		if err != nil {
+			return diag.FromErr(errors.New(fmt.Sprint("Failed to drop login: ", err)))
+		}
+
+		row, _ := checkDatabase(db, database)
+		if row != nil {
+			row, _ := checkUser(db, database, name)
+			if row != nil {
+				_, err = db.Query(fmt.Sprintf("exec('use %s; drop user %s');", database, name))
+				if err != nil {
+					return diag.FromErr(errors.New(fmt.Sprint("Failed to drop user: ", err)))
+				}
+			}
+		}
+
+		// check if user exists
+		//SELECT [name]
+		//FROM [Beratungsmappe].[sys].[database_principals]
+		//WHERE [type] = N'S'
+
+		//		row = db.Query(fmt.Sprintf("SELECT name FROM sys.server_principals WHERE principal_id = %s", d.Id()))
+
+		//_, err = db.Query(fmt.Sprintf("exec('use %s; drop user %s');", database, name))
+		//if err != nil {
+		//	return errors.New(fmt.Sprint("Failed to drop user: ", err))
+		//}
+
+	}
+
+	return nil
+}
